6c67abbe4e8d3440046077e4728f8076932383b0
kalpesh <kalpesh162@gmail.com>
2026-05-27 10:40:28 +0530

    Spring form-1

---
+++ SpringForm-1/src/main/java/com/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bean.Student;
+
+@Controller
+public class StudentController {
+
+	@RequestMapping("/")
+	public String welcomePage() {
+		return "index";
+	}
+
+	@RequestMapping("/open")
+	public String openForm(Model model) {
+		Student student=new Student();
+		model.addAttribute("stud", student);
+		return "studform";
+	}
+
+	
+	
+	@RequestMapping(value = "/show", method = RequestMethod.POST)
+	public String showData(@ModelAttribute("stud")Student student) {
+			//model.addAttribute("stud", student);
+		System.out.println(student);
+		return "display";
+	}
+
+}
